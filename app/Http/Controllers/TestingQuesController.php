ca568db283a56307743c4b9fa3850da9fe9b7b9f
Halim Teguh S <63193828+HalimTeguh@users.noreply.github.com>
2025-07-08 17:11:45 +0700

    Update TestingQuesController.php

--- app/Http/Controllers/TestingQuesController.php
+++ app/Http/Controllers/TestingQuesController.php
@@ -839,7 +839,6 @@ PROMPT;
 
             $result = StatisticalTestHelper::independentTTestManual($experiment, $control);
 
-            dd($result, $control, $experiment);
             
             // Simpan hasil ke DB
             IndependentTTest::updateOrCreate(
